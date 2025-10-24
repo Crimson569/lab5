a1df87a6b9c426be3964cdc1bb0b6f4f1dcacdfe
Тварковский Николай Вячеславович <80266130+Crimson569@users.noreply.github.com>
2025-10-24 08:22:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const accordions = document.querySelectorAll(".accordion-item");
+
+    accordions.forEach(item => {
+        const header = item.querySelector(".accordion-header");
+
+        header.addEventListener("click", () => {
+            // закрываем все остальные
+            accordions.forEach(i => {
+                if (i !== item) i.classList.remove("active");
+            });
+
+            // переключаем текущее
+            item.classList.toggle("active");
+        });
+    });
+});
